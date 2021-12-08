293c44562bc507281d903c9bbadbde532767f203
Léo Mainard <leomainard63@gmail.com>
2021-12-08 10:26:27 +0100

    Exercice 1

---
+++ src/com/company/Division.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Division {
+
+
+    public double division1(int a, int b){
+        float resultat = a/b;
+        return resultat;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,22 @@ package com.company;
 
 public class Main {
 
+    public static void exercice1(){
+        Division d = new Division();
+        d.division1(10,0);
+
+        // Erreur ici
+        /*Exception in thread "main" java.lang.ArithmeticException: / by zero
+        at com.company.Division.division1(Division.java:7)
+        at com.company.Main.exercice1(Main.java:7)
+        at com.company.Main.main(Main.java:14)*/
+    }
+
+
+
+
     public static void main(String[] args) {
-	// write your code here
+        exercice1();
 
     }
 }
